ed325570f30a911e21fcab32db8578860174bdf2
Brandon Fowler <brandon@fivefowlers.com>
2023-03-13 10:40:44 -0600

    Graphing nearly finished

--- src/main/java/com/freedommuskrats/fineengine/util/TerminalGraph.java
+++ src/main/java/com/freedommuskrats/fineengine/util/TerminalGraph.java
@@ -30,23 +30,25 @@ public class TerminalGraph {
             xminMax[1] = Math.max(xminMax[1], values.size());
         });
 
-        graph = new char[36][9];
+        graph = new char[37][9];
         graph = initialize(graph);
 
         graph = makeBorder(graph);
 
         graph = graphPoints(graph, yValues, yminMax, xminMax);
 
-        graph = addGraphDecorations(graph, yminMax, xminMax );
+        graph = insetGraphWithBorder(graph, 16, yValues.size() + 3);
+
+        String[][] axisNumbers = determineAxisNumbers(graph, yminMax, xminMax );
+
+        graph = addGraphDecorations(graph, axisNumbers, 16, yValues.size() + 3,  new String[]{xLabel, yLabel});
 
         display(graph);
     }
 
     private char[][] initialize(char[][] g) {
         for (int x = 0; x < g.length; x++) {
-            for (int y = 0; y < g[x].length; y++) {
-                g[x][y] = ' ';
-            }
+            Arrays.fill(g[x], ' ');
         }
         return g;
     }
@@ -74,8 +76,11 @@ public class TerminalGraph {
         final int[] c = {0};
         yValues.forEach((label, values) -> {
             for(int x = 1; x <= columns; x++) {
-                int y = getYValueForX(values, x, yStep, xStep);
-                g[x * 4][y] = graphIcons[c[0]];
+                int index = (int) Math.round(x * xStep);
+                if (index <= values.size()) {
+                    int y = getYValueForX(values, yStep, index);
+                    g[x * 4][y] = graphIcons[c[0]];
+                }
             }
             c[0] += 1;
         });
@@ -85,9 +90,8 @@ public class TerminalGraph {
 
     private int getYValueForX(
             List<Double> yValues,
-            int x,
             double yStep,
-            double xStep)
+            int index)
     {
         //10, 4
         // 1.25, .5
@@ -96,18 +100,98 @@ public class TerminalGraph {
         //100, 200, 300, 400
         // 1, 1, 2, 2, 3, 4, 5, 6, 7, 8
 
-        int index = (int) Math.round(x * xStep);
-        double average = yValues.get(index - 1) +
-                ((yValues.size() > index)? yValues.get(index) : yValues.get(index - 1));
-        average /= 2;
+//        double average = yValues.get(index - 1) +
+//                ((yValues.size() > index)? yValues.get(index) : yValues.get(index - 1));
+//        average /= 2.0;
+        double average = yValues.get(index - 1);
 
-        return (int) Math.round(average * yStep);
+        return (int) Math.round(average / yStep);
     }
 
+    private char[][] insetGraphWithBorder(char[][] graph, int leftBorder, int bottomBorder) {
+        char[][] newGraph = new char[leftBorder + graph.length][graph[0].length + bottomBorder];
+        newGraph = initialize(newGraph);
 
-    private char[][] addGraphDecorations(char[][] graph, double[] yminMax, double[] xminMax) {
+        for(int x = 0; x < graph.length; x++) {
+            for (int y = graph[0].length - 1; y >= 0; y--) {
+                newGraph[x + leftBorder][y + bottomBorder] = graph[x][y];
+            }
+        }
+
+        return newGraph;
+    }
+
+    private String[][] determineAxisNumbers(char[][] graph, double[] yminMax, double[] xminMax) {
+        String[] yLabels = new String[5];
+        String[] xLabels = new String[5];
+
+        if (yminMax[1] > 1000000) {
+            yminMax[0] = round(yminMax[0] / 1000, 1);
+            yminMax[1] = round(yminMax[1] / 1000, 1);
+
+            double yStep = (yminMax[1] - yminMax[0]) / 4;
+
+            yLabels[0] = round(yminMax[0], 1) + "m";
+            yLabels[1] = round(yminMax[0] + yStep, 1) + "m";
+            yLabels[2] = round(yminMax[0] + yStep + yStep, 1) + "m";
+            yLabels[3] = round(yminMax[1] - yStep, 1) + "m";
+            yLabels[4] = round(yminMax[1], 1) + "m";
+        }
+        else if (yminMax[1] > 1000) {
+            yminMax[0] = round(yminMax[0] / 1000, 1);
+            yminMax[1] = round(yminMax[1] / 1000, 1);
+
+            double yStep = (yminMax[1] - yminMax[0]) / 4;
+
+            yLabels[0] = round(yminMax[0], 1) + "k";
+            yLabels[1] = round(yminMax[0] + yStep, 1) + "k";
+            yLabels[2] = round(yminMax[0] + yStep + yStep, 1) + "k";
+            yLabels[3] = round(yminMax[1] - yStep, 1) + "k";
+            yLabels[4] = round(yminMax[1], 1) + "k";
+        }
+        else {
+            double yStep = (yminMax[1] - yminMax[0]) / 4;
+
+            yLabels[0] = String.valueOf(round(yminMax[0], 1));
+            yLabels[1] = String.valueOf(round(yminMax[0] + yStep, 1));
+            yLabels[2] = String.valueOf(round(yminMax[0] + yStep + yStep, 1));
+            yLabels[3] = String.valueOf(round(yminMax[1] - yStep, 1));
+            yLabels[4] = String.valueOf(round(yminMax[1], 1));
+        }
+
+
+
+        double xStep = (xminMax[1] - xminMax[0]) / 5;
+        xLabels[0] = String.valueOf(round(xminMax[0], 1));
+        xLabels[1] = String.valueOf(round(xminMax[0] + xStep, 1));
+        xLabels[2] = String.valueOf(round(xminMax[0] + xStep + xStep, 1));
+        xLabels[3] = String.valueOf(round(xminMax[1] - xStep, 1));
+        xLabels[4] = String.valueOf(round(xminMax[1], 1));
+
+        return new String[][]{xLabels, yLabels};
+    }
+
+    private char[][] addGraphDecorations(char[][] graph, String[][] axisNumbers, int leftBorder, int bottomBorder, String[] xyLabels) {
+        for (int label = 0; label < axisNumbers[1].length; label++) {
+            String ylabel = axisNumbers[1][label];
+            int l = ylabel.length();
+            int index = leftBorder - l;
+            for (int i = index; i < leftBorder; i++) {
+                graph[i][label * 2 + bottomBorder] = ylabel.charAt(i - index);
+            }
+        }
+
+        for (int label = 0; label < axisNumbers[0].length; label++) {
+            String xLabel = axisNumbers[0][label];
+            int index = leftBorder + label * 8;
+            for (int i = 0; i < xLabel.length(); i++) {
+                graph[index + i][bottomBorder - 1] = xLabel.charAt(i);
+            }
+        }
+
         return graph;
     }
+
     private void display(char[][] g) {
         for (int y = g[0].length - 1; y >= 0; y--) {
             for (int x = 0; x < g.length; x++) {
--- src/test/java/com/freedommuskrats/fineengine/util/TerminalGraphTest.java
+++ src/test/java/com/freedommuskrats/fineengine/util/TerminalGraphTest.java
@@ -1,5 +1,9 @@
 package com.freedommuskrats.fineengine.util;
 
+import com.freedommuskrats.fineengine.dal.models.TimeUnit;
+import com.freedommuskrats.fineengine.dal.models.investments.Fund;
+import com.freedommuskrats.fineengine.service.projections.Projection;
+import com.freedommuskrats.fineengine.service.projections.ProjectionLine;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -7,17 +11,42 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.freedommuskrats.fineengine.util.AnnuityMath.buildContributionSchedule;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TerminalGraphTest {
 
     @Test
     void test_TerminalGraph() {
         Map<String, List<Double>> yValues = new LinkedHashMap<>();
-        yValues.put("EFT", new ArrayList<Double>(
-                    List.of(0.0, 0.5, 2.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0)
+        yValues.put("EFT", new ArrayList<>(
+                    List.of(1000.0, 4000.0, 8000.0, 16000.0, 32000.0, 64000.0)
                 )
         );
+
+        TerminalGraph graph = new TerminalGraph("Value", "Years", yValues);
+    }
+
+    @Test
+    void test_TerminalGraph_withRealAnnuity() {
+        Fund fund = new Fund(6, 2000, "EFT", null, null, null);
+        Projection projection = fund.makeProjection(
+                fund.getCurrentValue(),
+                1000,
+                TimeUnit.MONTH,
+                false,
+                TimeUnit.YEAR,
+                15);
+
+        List<ProjectionLine> lines = projection.getLines();
+
+
+        Map<String, List<Double>> yValues = new LinkedHashMap<>();
+        List<Double> values = new ArrayList<>();
+        for (ProjectionLine line : lines) {
+            values.add(line.getEndBalance());
+        }
+        yValues.put("EFT", values);
+
 
         TerminalGraph graph = new TerminalGraph("Value", "Years", yValues);
     }
